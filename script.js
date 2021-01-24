c9a86b358463e257f5ab56358d70c7b56a6c3212
arzzcom <arzz@arzz.com>
2021-01-24 19:37:40 +0900

    메시지 처리방식 변경에 따른 수정

--- script.js
+++ script.js
@@ -7,8 +7,10 @@
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 1.0.0
- * @modified 2020. 12. 28.
+ * @modified 2021. 1. 21.
  */
+if (Minitalk === undefined) return;
+
 Minitalk.on("connect",function(minitalk) {
 	minitalk.ui.printSystemMessage("plugin","[주사위 플러그인] 채팅창에 /주사위 [돌릴 숫자]를 쳐보세요. 주사위 결과가 출력 됩니다.");
 });
@@ -20,9 +22,33 @@ Minitalk.on("command",function(minitalk,command,commands) {
 		
 		var dice = Math.floor((Math.random() * (limit - 1))) + 1;
 		
-		Minitalk.socket.sendMessage("/주사위 " + limit);
-		Minitalk.socket.sendMessage("주사위를 굴림결과 : [" + dice + "]");
+		Minitalk.socket.sendMessage("dice","/주사위 " + limit,{result:dice});
 		
 		return true;
 	}
+});
+
+// 사용자정의 메시지 타입 이벤트를 받아, 화면에 보여준다.
+Minitalk.on("printMessage",function(minitalk,message,$content) {
+	/**
+	 * 내용을 담을 말풍선을 생성한다.
+	 */
+	var $balloon = $("<div>").attr("data-role","balloon");
+	$balloon.append($("<span>").addClass("text").html(Minitalk.ui.decodeMessage(message.message)));
+	
+	if (message.time === undefined) {
+		$balloon.append($("<span>").addClass("time").html('<i class="sending"></i>'));
+	} else {
+		$balloon.append($("<span>").addClass("time").html($("<time>").attr("datetime",message.time).html(Minitalk.ui.getTime(message.time,Minitalk.dateFormat))));
+	}
+	
+	$content.append($balloon);
+	
+	/**
+	 * 주사위 굴림 결과를 보여준다.
+	 */
+	var $result = $("<div>").attr("data-role","dice").css("fontSize","12px").css("color","green");
+	$result.append("주사위 굴림 결과 <b>" + message.data.result + "</b> 이 나왔습니다.");
+	
+	$content.append($result);
 });
